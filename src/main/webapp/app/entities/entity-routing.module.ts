ff81118ba466d60c59cfc0cb4eb50e2b1971f493
Oleksandr Yevdokymov <o.yevdokymov@khai.edu>
2023-10-19 16:27:31 +0300

    Add CRUD for cosmetics store

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,36 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'product',
+        data: { pageTitle: 'testApp.product.home.title' },
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+      },
+      {
+        path: 'category',
+        data: { pageTitle: 'testApp.category.home.title' },
+        loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
+      },
+      {
+        path: 'customer',
+        data: { pageTitle: 'testApp.customer.home.title' },
+        loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
+      },
+      {
+        path: 'order',
+        data: { pageTitle: 'testApp.order.home.title' },
+        loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
+      },
+      {
+        path: 'order-item',
+        data: { pageTitle: 'testApp.orderItem.home.title' },
+        loadChildren: () => import('./order-item/order-item.module').then(m => m.OrderItemModule),
+      },
+      {
+        path: 'review',
+        data: { pageTitle: 'testApp.review.home.title' },
+        loadChildren: () => import('./review/review.module').then(m => m.ReviewModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
